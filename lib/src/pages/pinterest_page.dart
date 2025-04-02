c8ccf9647c17c5ed50f9e6f2aada33cfc788b0ab
DeveTe5t <devcurses@gmail.com>
2025-04-01 22:41:24 -0600

    Remove comment code and solutions not useful

--- lib/src/pages/pinterest_page.dart
+++ lib/src/pages/pinterest_page.dart
@@ -6,7 +6,7 @@ class PinterestPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: PinterestGrid()));
+    return const Scaffold(body: PinterestGrid());
   }
 }
 
@@ -15,85 +15,23 @@ class PinterestGrid extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // return AlignedGridView.count(
-    //   crossAxisCount: 4,
-    //   mainAxisSpacing: 4,
-    //   crossAxisSpacing: 4,
-    //   itemBuilder: (context, index) {
-    //     return StaggeredGridTile.count(
-    //       crossAxisCellCount: 2,
-    //       mainAxisCellCount: 2,
-    //       child: Text('${(index % 7 + 1) * 30}'),
-    //     );
-    //   },
-    // );
-    // return MasonryGridView.count(
-    //   crossAxisCount: 4,
-    //   mainAxisSpacing: 4,
-    //   crossAxisSpacing: 4,
-    //   itemBuilder: (context, index) {
-    //     // return Tile(index: index, extent: (index % 5 + 1) * 100);
-    //     return StaggeredGridTile.extent(
-    //       mainAxisExtent: (index % 5 + 1) * 100,
-    //       crossAxisCellCount: 2,
-    //       child: GridTile(child: Text('$index')),
-    //     );
-    //   },
-    // );
-
-    // return StaggeredGrid.count(
-    //   crossAxisCount: 4,
-    //   mainAxisSpacing: 4,
-    //   crossAxisSpacing: 4,
-    //   children: const [
-    //     StaggeredGridTile.count(
-    //       crossAxisCellCount: 2,
-    //       mainAxisCellCount: 2,
-    //       // child: GridTile(index: 0),
-    //       child: GridTile(child: Text('0')),
-    //     ),
-    //     StaggeredGridTile.count(
-    //       crossAxisCellCount: 2,
-    //       mainAxisCellCount: 1,
-    //       // child: GridTile(index: 1),
-    //       child: GridTile(child: Text('1')),
-    //     ),
-    //     StaggeredGridTile.count(
-    //       crossAxisCellCount: 1,
-    //       mainAxisCellCount: 1,
-    //       // child: Tile(index: 2),
-    //       child: GridTile(child: Text('2')),
-    //     ),
-    //     StaggeredGridTile.count(
-    //       crossAxisCellCount: 1,
-    //       mainAxisCellCount: 1,
-    //       // child: Tile(index: 3),
-    //       child: GridTile(child: Text('3')),
-    //     ),
-    //     StaggeredGridTile.count(
-    //       crossAxisCellCount: 4,
-    //       mainAxisCellCount: 2,
-    //       // child: Tile(index: 4),
-    //       child: GridTile(child: Text('4')),
-    //     ),
-    //   ],
-    // );
-
     final List<int> items = List.generate(200, (index) => index);
 
+    // ********** With package
     //------- Solution 1
-    return MasonryGridView.count(
-      crossAxisCount: 2,
-      mainAxisSpacing: 4,
-      crossAxisSpacing: 4,
-      padding: const EdgeInsets.all(10),
-      itemCount: items.length,
-      itemBuilder:
-          // (context, index) => _PinterestItem2(index: index),
-          // (context, index) => _PinterestItem(index: index),
-          (context, index) =>
-              _Tile(index: index, extent: (index % 5 + 1) * 100),
-    );
+    // return Container(
+    //   padding: const EdgeInsets.symmetric(horizontal: 20),
+    //   child: MasonryGridView.count(
+    //     crossAxisCount: 2,
+    //     mainAxisSpacing: 4,
+    //     crossAxisSpacing: 4,
+    //     // padding: const EdgeInsets.all(10),
+    //     itemCount: items.length,
+    //     itemBuilder: (context, index) {
+    //       return _Tile(index: index, extent: (index % 5 + 1) * 100);
+    //     },
+    //   ),
+    // );
 
     //------- Solution 2
     // return SingleChildScrollView(
@@ -105,11 +43,10 @@ class PinterestGrid extends StatelessWidget {
     //       items.length,
     //       (index) => _PinterestItem2(index: index),
     //     ),
-    //     // staggeredTitleBuilder:
-    //     //     (int index) => StaggeredTitle.count(2, index.isEven ? 2 : 3),
     //   ),
     // );
 
+    // ********** Without package
     //------- Solution 3
     // return GridView.custom(
     //   padding: const EdgeInsets.symmetric(horizontal: 10),
@@ -131,34 +68,43 @@ class PinterestGrid extends StatelessWidget {
     // );
 
     //------- Solution 4
-    // return GridView.custom(
-    //   padding: const EdgeInsets.symmetric(horizontal: 10),
-    //   gridDelegate: SliverQuiltedGridDelegate(
-    //     crossAxisCount: 4,
-    //     mainAxisSpacing: 4,
-    //     crossAxisSpacing: 4,
-    //     repeatPattern: QuiltedGridRepeatPattern.inverted,
-    //     pattern: const [QuiltedGridTile(3, 2), QuiltedGridTile(2, 2)],
-    //   ),
-    //   childrenDelegate: SliverChildBuilderDelegate(
-    //     (context, index) => _PinterestItem(index: index),
-    //     childCount: items.length,
-    //   ),
-    // );
+    return GridView.custom(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      gridDelegate: SliverQuiltedGridDelegate(
+        crossAxisCount: 4,
+        mainAxisSpacing: 4,
+        crossAxisSpacing: 4,
+        repeatPattern: QuiltedGridRepeatPattern.inverted,
+        pattern: const [QuiltedGridTile(3, 2), QuiltedGridTile(2, 2)],
+      ),
+      childrenDelegate: SliverChildBuilderDelegate(
+        (context, index) => _PinterestItem(index: index),
+        childCount: items.length,
+      ),
+    );
+  }
+}
 
-    //------- Solution 5
-    // return Container(
-    //   padding: const EdgeInsets.symmetric(horizontal: 20),
-    //   child: MasonryGridView.count(
-    //     crossAxisCount: 2,
-    //     mainAxisSpacing: 4,
-    //     crossAxisSpacing: 4,
-    //     itemCount: items.length,
-    //     itemBuilder: (context, index) {
-    //       return _Tile(index: index, extent: (index % 5 + 1) * 100);
-    //     },
-    //   ),
-    // );
+class _Tile extends StatelessWidget {
+  const _Tile({required this.index, required this.extent});
+  final int index;
+  final double extent;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: extent,
+      decoration: const BoxDecoration(
+        color: Colors.blue,
+        borderRadius: BorderRadius.all(Radius.circular(30)),
+      ),
+      child: Center(
+        child: CircleAvatar(
+          backgroundColor: Colors.white,
+          child: Text('$index'),
+        ),
+      ),
+    );
   }
 }
 
@@ -169,7 +115,6 @@ class _PinterestItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      // color: Colors.blue,
       // margin: const EdgeInsets.all(5),
       decoration: const BoxDecoration(
         color: Colors.blue,
@@ -195,7 +140,6 @@ class _PinterestItem2 extends StatelessWidget {
       crossAxisCellCount: 2,
       mainAxisCellCount: index.isEven ? 2 : 3,
       child: Container(
-        // color: Colors.blue,
         margin: const EdgeInsets.all(5),
         decoration: const BoxDecoration(
           color: Colors.blue,
@@ -206,29 +150,6 @@ class _PinterestItem2 extends StatelessWidget {
             backgroundColor: Colors.white,
             child: Text('$index'),
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class _Tile extends StatelessWidget {
-  const _Tile({required this.index, required this.extent});
-  final int index;
-  final double extent;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: extent,
-      decoration: const BoxDecoration(
-        color: Colors.blue,
-        borderRadius: BorderRadius.all(Radius.circular(30)),
-      ),
-      child: Center(
-        child: CircleAvatar(
-          backgroundColor: Colors.white,
-          child: Text('$index'),
         ),
       ),
     );
